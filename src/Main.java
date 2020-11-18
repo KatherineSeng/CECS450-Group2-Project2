8c82880febe1364e5ac21157933b50c522621379
Katherine Seng <43624009+KatherineSeng@users.noreply.github.com>
2020-11-18 01:15:50 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,47 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) {
-        String directory = System.getProperty("user.dir");
-        int totalFolders = findFolders(directory);
+    public static void main(String[] args) throws IOException {
+        //String directory = System.getProperty("user.dir");
+        //int totalFolders = findFolders(directory);
 
-        int i = 0;
-        while (totalFolders != 0) {
+        int totalParticipants = 36;
+
+        for (int i = 1; i <= totalParticipants; i++) {
             String folder = "p" + i;
+
+            File dir = new File(".");
+            File parentDir = dir.getParentFile();
 
             // Checks if the folder exists
             if (Files.exists(Paths.get(folder))) {
-                /**
-                 * Iterate through the contents of the folder
-                 * Extract the data
-                 */
+                // Graph ("indented list") fixation data
+                File graphFXD = new File(parentDir,folder + "/" + folder + ".graphFXD.txt");
+                // Tree ("graph") fixation data
+                File treeFXD = new File(parentDir,folder + "/" + folder + ".treeFXD.txt");
 
-                // Folder checked, move to the next one
-                totalFolders--;
+                if (graphFXD.canRead() && treeFXD.canRead()) {
+                    // Extracts data from graph
+                    // Extracts data from tree
+                }
             }
-            i++;
         }
     }
 
     // Method to find the raw data folders in the directory
-    public static int findFolders(String folder) {
-        File directory = new File(folder);
-        int numFolders = 0;
-        File listDirectory[] = directory.listFiles();
-        for (int i = 0; i < listDirectory.length; i++) {
-            if (listDirectory[i].isDirectory()) {
-                numFolders++;
-            }
-        }
+    //public static int findFolders(String folder) {
+        //File directory = new File(folder);
+        //int numFolders = 0;
+        //File listDirectory[] = directory.listFiles();
+        //for (int i = 0; i < listDirectory.length; i++) {
+            //if (listDirectory[i].isDirectory()) {
+                //numFolders++;
+            //}
+        //}
         // Returns only the number of raw data folders
-        return numFolders - 4;
-    }
+        //return numFolders - 4;
+    //}
 }
