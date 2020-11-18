8e2fcc26b5490db9f27727eacba07de62c88c2f8
Pikakashi <47730246+Pikakashi@users.noreply.github.com>
2020-11-18 14:16:43 -0800

    Create Participant.java

---
+++ src/Participant.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Participant {
+	
+	public ArrayList<FixPoint> graph, tree;
+	
+	public Participant()
+	{
+		graph = new ArrayList<FixPoint>();
+		tree = new ArrayList<FixPoint>();
+	}
+	
+}
