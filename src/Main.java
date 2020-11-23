d698df712b014b12008aea9127b12d580ad35819
Katherine Seng <43624009+KatherineSeng@users.noreply.github.com>
2020-11-23 13:21:35 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,74 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //String directory = System.getProperty("user.dir");
-        //int totalFolders = findFolders(directory);
-
         int totalParticipants = 36;
 
-        for (int i = 1; i <= 1; i++) {
-            String folder = "p" + i;
+        JSONObject jsonObject;
+        JSONArray jsonArray;
 
-            File dir = new File(".");
-            File parentDir = dir.getParentFile();
+        File dir = new File(".");
+        File parentDir = dir.getParentFile();
 
-            HashMap<String, List<Integer>> treeMap = new HashMap<>();
-            HashMap<String, List<Integer>> graphMap = new HashMap<>();
+        for (int i = 1; i <= totalParticipants; i++) {
+            String pFolder = "p" + i;
 
-            // Checks if the folder exists
-            if (Files.exists(Paths.get(folder))) {
-                // Graph ("indented list") fixation data
-                File graphFXD = new File(parentDir,folder + "/" + folder + ".graphFXD.txt");
+            // Checks if the raw data folder exists
+            if (Files.exists(Paths.get(pFolder))) {
+                // Creates the FileWriter objects for the graph and tree files
+                FileWriter gFile = new FileWriter(pFolder + "_Graph.json");
+                FileWriter tFile = new FileWriter(pFolder + "_Tree.json");
+
+                File graphFXD = new File(parentDir, pFolder + "/" + pFolder + ".graphFXD.txt");
                 Scanner graphReader = new Scanner(graphFXD);
-                List<Integer> graphList = new ArrayList<>();
-                // Tree ("graph") fixation data
-                File treeFXD = new File(parentDir,folder + "/" + folder + ".treeFXD.txt");
-                Scanner treeReader = new Scanner(treeFXD);
-                List<Integer> treeList = new ArrayList<>();
 
+                File treeFXD = new File(parentDir, pFolder + "/" + pFolder + ".treeFXD.txt");
+                Scanner treeReader = new Scanner(treeFXD);
+
+                // Checks if the folder exists
                 if (graphFXD.canRead() && treeFXD.canRead()) {
+                    jsonArray = new JSONArray();
+                    // Adds raw data from the Graph FXD to a json object
                     while (graphReader.hasNext()) {
-                        graphList.add(Integer.parseInt(graphReader.next()));
+                        jsonObject = new JSONObject();
+                        jsonObject.put("Fix Point", Integer.parseInt(graphReader.next()));
+                        jsonObject.put("Start Time", Integer.parseInt(graphReader.next()));
+                        jsonObject.put("Length", Integer.parseInt(graphReader.next()));
+                        jsonObject.put("X Pos", Integer.parseInt(graphReader.next()));
+                        jsonObject.put("Y Pos", Integer.parseInt(graphReader.next()));
+                        // Adds the json object to the json array
+                        jsonArray.put(jsonObject);
                     }
+                    // Writes the contents of the json array to the file
+                    gFile.write(jsonArray.toString());
+
+                    jsonArray = new JSONArray();
+                    // Adds raw data from the Tree FXD to a json object
                     while (treeReader.hasNext()) {
-                        treeList.add(Integer.parseInt(treeReader.next()));
+                        jsonObject = new JSONObject();
+                        jsonObject.put("Fix Point", Integer.parseInt(treeReader.next()));
+                        jsonObject.put("Start Time", Integer.parseInt(treeReader.next()));
+                        jsonObject.put("Length", Integer.parseInt(treeReader.next()));
+                        jsonObject.put("X Pos", Integer.parseInt(treeReader.next()));
+                        jsonObject.put("Y Pos", Integer.parseInt(treeReader.next()));
+                        // Adds the json object to the json array
+                        jsonArray.put(jsonObject);
                     }
-                    graphMap.put(folder, graphList);
-                    treeMap.put(folder, treeList);
+                    // Writes the contents of the json array to the file
+                    tFile.write(jsonArray.toString());
                 }
-                //System.out.println(treeMap.get(folder));
-                //System.out.println(graphMap.get(folder));
-                graphReader.close();
-                treeReader.close();
+                // Closes the files
+                gFile.close();
+                tFile.close();
             }
         }
     }
-
-    // Method to find the raw data folders in the directory
-    //public static int findFolders(String folder) {
-        //File directory = new File(folder);
-        //int numFolders = 0;
-        //File listDirectory[] = directory.listFiles();
-        //for (int i = 0; i < listDirectory.length; i++) {
-            //if (listDirectory[i].isDirectory()) {
-                //numFolders++;
-            //}
-        //}
-        // Returns only the number of raw data folders
-        //return numFolders - 4;
-    //}
 }
